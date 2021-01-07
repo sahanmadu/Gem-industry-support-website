4130401ab224ab5b510d6d027fd04a5b6d2837d0
Sahan Madusanka <44167777+sahanmadu@users.noreply.github.com>
2021-01-07 10:37:36 +0530

    added config

---
+++ config/constant.php
@@ -0,0 +1,10 @@
+<?php
+
+define('EMAIL','smgemselling@gmail.com');
+define('PASSWORD','Madu2$SMT');
+
+ 
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','Atuhuser');
